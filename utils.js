fb8201aa44ec65e3f07a7d2f6f01aed82b9c231f
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2023-03-04 17:22:56 -0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,29 @@
+function number_format(numero, decimais, separadorDecimal, separadorMilhar) {
+    var n = numero,
+        c = isNaN(decimais = Math.abs(decimais)) ? 2 : decimais,
+        d = separadorDecimal == undefined ? "," : separadorDecimal,
+        t = separadorMilhar == undefined ? "." : separadorMilhar,
+        s = n < 0 ? "-" : "",
+        i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+        j = (j = i.length) > 3 ? j % 3 : 0;
+
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
+
+function mascaraDinheiro(valor) {
+    valor = valor.replace(/\D/g, ""); // Remove tudo o que não é dígito
+    valor = valor.replace(/^0+/, ""); // Remove os zeros à esquerda
+    valor = valor.padStart(3, "0"); // Adiciona zeros à esquerda, se necessário
+    valor = valor.replace(/(\d{2})$/, ",$1"); // Adiciona vírgula para os centavos
+    valor = valor.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."); // Adiciona ponto para os milhares
+    valor = valor.replace(/\.(\d{2})$/, ",$1"); // Corrige a vírgula dos centavos quando o usuário digita um ponto
+
+    // Adiciona zero na unidade se o valor tiver somente os centavos
+    if (/^[1-9]$/.test(valor)) {
+      valor = "0,0" + valor;
+    } else if (/^\d{1,2}$/.test(valor)) {
+      valor = "0," + valor.padStart(2, "0");
+    }
+
+    return valor;
+  }
