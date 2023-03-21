009db0acf9ff3c7df762f6234ec3f12de32f29b2
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2023-03-21 16:41:12 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,38 @@
+function limparString(input) {
+    // Substitui caracteres com acentos por caracteres sem acentos
+    const mapaAcentosHex = {
+        a: /[\xE0-\xE6]/g,
+        e: /[\xE8-\xEB]/g,
+        i: /[\xEC-\xEF]/g,
+        o: /[\xF2-\xF6]/g,
+        u: /[\xF9-\xFC]/g,
+        c: /\xE7/g,
+        n: /\xF1/g,
+    };
+
+    for (let letra in mapaAcentosHex) {
+        const expressaoRegular = mapaAcentosHex[letra];
+        input = input.replace(expressaoRegular, letra);
+    }
+
+    // Substitui todos os caracteres especiais e espaços por vazio
+    return input.replace(/[^\w\s]/gi, '').replace(/\s+/g, '');
+}
+
+function mascaraCPFCNPJ(valor) {
+    valor = valor.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    if (valor.length <= 11) { // Se o valor tiver até 11 caracteres, trata-se de um CPF
+      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+      valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    } else { // Senão, trata-se de um CNPJ
+      valor = valor.replace(/^(\d{2})(\d)/, '$1.$2');
+      valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+      valor = valor.replace(/\.(\d{3})(\d)/, '.$1/$2');
+      valor = valor.replace(/(\d{4})(\d)/, '$1-$2');
+    }
+    return valor;
+}
 function mascaraCPFCNPJ(valor) {
   valor = valor.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
   if (valor.length <= 11) { // Se o valor tiver até 11 caracteres, trata-se de um CPF
