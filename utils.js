9f9d9dc2e7afeedbdba21d03de0126b00fe030f1
José Alisson <98172458+josealissonbr@users.noreply.github.com>
2023-04-30 15:36:59 -0300

    toFixedTrunc() & toFixed()

--- utils.js
+++ utils.js
@@ -1,3 +1,33 @@
+function toFixedTrunc(x, n) {
+    x = toFixed(x)
+
+    // From here on the code is the same than the original answer
+    const v = (typeof x === 'string' ? x : x.toString()).split('.');
+    if (n <= 0) return v[0];
+    let f = v[1] || '';
+    if (f.length > n) return `${v[0]}.${f.substr(0,n)}`;
+    while (f.length < n) f += '0';
+    return `${v[0]}.${f}`
+}
+
+function toFixed(x) {
+    if (Math.abs(x) < 1.0) {
+      let e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+          x *= Math.pow(10,e-1);
+          x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+      }
+    } else {
+      let e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+          e -= 20;
+          x /= Math.pow(10,e);
+          x += (new Array(e+1)).join('0');
+      }
+    }
+    return x;
+}
+
 function formatCEP(cep) {
   // Remove tudo que não for dígito
   cep = cep.replace(/\D/g, '');
